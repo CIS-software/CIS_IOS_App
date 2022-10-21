aeb3a454c01607a342bab6af2d8ef7c3994cd35e
Jok-ok <nujniy.vorobey@gmail.com>
2022-10-21 21:17:17 +0200

    Initial Commit

---
+++ CISApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CISApp
+//
+//  Created by Александр Воробей on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
