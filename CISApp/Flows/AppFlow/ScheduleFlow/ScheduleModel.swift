46072b1ec794d58eaa4fbebd07168a0ce88ade77
Jok-ok <nujniy.vorobey@gmail.com>
2023-03-21 14:02:23 +0200

    ScheduleModel adding

---
+++ CISApp/Flows/AppFlow/ScheduleFlow/ScheduleModel.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct Schedule {
+    var days: [TrainDay]?
+}
+
+
+struct TrainDay {
+    var day: String?
+    var description: String?
+}
---
+++ CISApp/Flows/AppFlow/ScheduleFlow/ScheduleTableViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ScheduleTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
